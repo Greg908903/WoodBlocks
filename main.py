573770bcd1d2a0dafbe3fad1ca141d70553d0739
Greg908903 <grisha.novgorodtsev@gmail.com>
2022-02-21 21:09:52 +0300

    Хоть какое-то начало проекта)

---
+++ main.py
@@ -0,0 +1,60 @@
+import pygame
+import pygame_gui
+import sys
+import os
+
+FPS = 50
+
+pygame.init()
+size = WIDTH, HEIGHT = 835, 1059
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+manager = pygame_gui.UIManager((WIDTH, HEIGHT))
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def start_screen():
+
+    fon = pygame.transform.scale(load_image('fon.png'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
+                                                text='Say Hello',
+                                                manager=manager)
+    while True:
+        time_delta = clock.tick(60) / 1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == hello_button:
+                    return
+            manager.process_events(event)
+
+        manager.update(time_delta)
+        manager.draw_ui(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+start_screen()
