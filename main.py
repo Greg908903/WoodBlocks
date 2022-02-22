0d34f8102b9eb845fe0e01e456734ecbd6369436
Greg908903 <grisha.novgorodtsev@gmail.com>
2022-02-22 19:04:51 +0300

    Чё-то написал

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 FPS = 50
 
 pygame.init()
-size = WIDTH, HEIGHT = 835, 1059
+size = WIDTH, HEIGHT = 600, 800
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 manager = pygame_gui.UIManager((WIDTH, HEIGHT), 'data\\theme.json')
@@ -33,13 +33,18 @@ def load_image(name, colorkey=None):
         image = image.convert_alpha()
     return image
 
+def show_statistic():
+    pass
 
 def start_screen():
 
     fon = pygame.transform.scale(load_image('fon.png'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
-    hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((342, 300), (150, 75)),
+    start_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((342, 300), (150, 75)),
                                                 text='Начать игру',
+                                                manager=manager)
+    statistic_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((342, 400), (150, 75)),
+                                                text='Статистика',
                                                 manager=manager)
 
     while True:
@@ -48,8 +53,10 @@ def start_screen():
             if event.type == pygame.QUIT:
                 terminate()
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
-                if event.ui_element == hello_button:
+                if event.ui_element == start_button:
                     return
+                if event.ui_element == statistic_button:
+                    show_statistic()
             manager.process_events(event)
 
         manager.update(time_delta)
@@ -58,4 +65,46 @@ def start_screen():
         clock.tick(FPS)
 
 
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, screen):
+        for i in range(self.height):
+            for j in range(self.width):
+                x = self.left + j * self.cell_size
+                y = self.top + i * self.cell_size
+                pygame.draw.rect(screen, (91, 35, 0), (x + 2, y + 2, self.cell_size - 4, self.cell_size - 4))
+
+
+def draw(screen, score):
+    screen.fill((0, 0, 0))
+    screen.blit(border_image, (0, 60))
+    screen.blit(wood, (0, 0))
+    font = pygame.font.Font(None, 50)
+    text = font.render(str(score), True, (156, 20, 20))
+    text_x = WIDTH // 2 - text.get_width() // 2
+    text_y = 30 - text.get_height() // 2
+    screen.blit(text, (text_x, text_y))
+
 start_screen()
+screen.fill((0, 0, 0))
+border_image = pygame.transform.scale(load_image('field_fon.png'), (WIDTH, WIDTH + 5))
+wood = load_image('wood.png')
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminate()
+    draw(screen, 0)
+    pygame.display.flip()
+    clock.tick(FPS)
